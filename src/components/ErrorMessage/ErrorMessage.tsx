70c35a42049007d00ed4503690ffe6f418a99143
Melike Bozoğlan <m.sultanbozoglan@gmail.com>
2025-11-19 15:15:48 +0300

    err comp revised

--- src/components/ErrorMessage/ErrorMessage.tsx
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -2,14 +2,21 @@
 
 interface ErrorMessageProps {
     message?: string;
+    onClose: () => void;
 };
 
-const ErrorMessage = ({ message = "Beklenmeyen bir hata oluştu." }: ErrorMessageProps) => {
+const ErrorMessage = ({ message = "Beklenmeyen bir hata oluştu.", onClose }: ErrorMessageProps) => {
     return (
         <div className="fixed inset-0 bg-white/70 backdrop-blur-sm flex items-center justify-center p-4 z-50">
-            <div className="bg-[#FFF0FF] border border-red-600 text-red-700 px-6 py-4 rounded-xl shadow-md max-w-sm w-full text-center">
-                <h2 className="font-semibold text-lg mb-1">Hata</h2>
-                <p className="text-sm">{message}</p>
+            <div className="bg-[#FFF0FF] border border-red-600 text-red-700 px-6 py-5 rounded-xl shadow-md max-w-sm w-full text-center flex flex-col items-center justify-center gap-10">
+                <div>
+                    <h2 className="font-semibold text-lg mb-1">Hata</h2>
+                    <p className="text-sm">{message}</p>
+                </div>
+
+                <button aria-label="" className="bg-red-700 border border-red-600 text-white px-2 py-2 rounded-xl shadow-md w-1/5 text-center" onClick={onClose} >
+                    <p className="text-sm">Tamam</p>
+                </button>
             </div>
         </div>
     );
